2498ad3f1d03c5bf04880a5001f519e4aba2be03
Michael Gremmelmaier <michael.gremmelmaier@hs-furtwangen.de>
2015-11-10 14:38:15 +0100

    Maven Projekt einfügen

---
+++ src/main/java/de/gremmelmaier/App.java
@@ -0,0 +1,17 @@
+package de.gremmelmaier;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String a = scan.nextLine();
+		a = a.toUpperCase();
+		System.out.println(a);
+    }
+}
